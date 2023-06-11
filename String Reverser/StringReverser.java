073b65a9c6839dcbc8b1c54a389dae0c7b44a48d
Kelvin Viana <kelvin.22viana@gmail.com>
2023-06-11 13:55:28 -0300

    Create StringReverser.java

---
+++ String Reverser/StringReverser.java
@@ -0,0 +1,21 @@
+class Reverse {
+    public static String reverse(String s) {
+        char[] chars = s.toCharArray();
+        int esquerda = 0;
+        int direita = chars.length - 1;
+        while (esquerda < direita){
+            char temp = chars[esquerda];
+            chars[esquerda] = chars[direita];
+            chars[direita] = temp;
+            esquerda++;
+            direita--;
+        } 
+        return new String(chars);
+    }
+}
+
+public class StringReverser {
+    public static void main(String[] args) {
+        Reverse.reverse("Abacaxi");
+    }
+}
